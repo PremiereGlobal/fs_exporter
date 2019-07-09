bb1a1173526f1a1d9546e6ae22d4ee48af3546fb
Luke Wahlmeier <lwahlmeier@gmail.com>
2019-07-09 16:45:23 -0600

    updating build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm  -rf build 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -mod=vendor -ldflags="-s -w" -o ./build/fs_exporter
+GO111MODULE=on CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -mod=vendor -ldflags="-s -w" -o ./build/fs_exporter
